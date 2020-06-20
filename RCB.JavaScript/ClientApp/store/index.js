bef3322d6b87ac58ae9bebf3bfc11354600aa937
Nikolay Maev aka Trixon <trix.mnr@gmail.com>
2020-06-20 18:11:01 +0300

    Update index.js

--- RCB.JavaScript/ClientApp/store/index.js
+++ RCB.JavaScript/ClientApp/store/index.js
@@ -1,5 +1,5 @@
-import * as LoginStore from "@Store/LoginStore";
-import * as PersonStore from "@Store/PersonStore";
+import * as LoginStore from "@Store/loginStore";
+import * as PersonStore from "@Store/personStore";
 
 // Whenever an action is dispatched, Redux will update each top-level application state property using
 // the reducer with the matching name. It's important that the names match exactly, and that the reducer
