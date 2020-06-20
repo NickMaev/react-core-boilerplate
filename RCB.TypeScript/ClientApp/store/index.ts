fe62c5a0800163db8d9070d494955531699a15bc
Nikolay Maev aka Trixon <trix.mnr@gmail.com>
2020-06-20 18:13:15 +0300

    Update index.ts

--- RCB.TypeScript/ClientApp/store/index.ts
+++ RCB.TypeScript/ClientApp/store/index.ts
@@ -1,19 +1,19 @@
-import * as LoginStore from "@Store/LoginStore";
-import * as PersonStore from "@Store/PersonStore";
+import * as loginStore from "@Store/loginStore";
+import * as personStore from "@Store/personStore";
 import { connect } from "react-redux";
 
 // The top-level state object.
 export interface IApplicationState {
-    login: LoginStore.ILoginStoreState;
-    person: PersonStore.IPersonStoreState;
+    login: loginStore.ILoginStoreState;
+    person: personStore.IPersonStoreState;
 }
 
 // Whenever an action is dispatched, Redux will update each top-level application state property using
 // the reducer with the matching name. It's important that the names match exactly, and that the reducer
 // acts on the corresponding ApplicationState property type.
 export const reducers = {
-    login: LoginStore.reducer,
-    person: PersonStore.reducer
+    login: loginStore.reducer,
+    person: personStore.reducer
 };
 
 // This type can be used as a hint on action creators so that its 'dispatch' and 'getState' params are
