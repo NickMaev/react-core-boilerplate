70a1fb9dffc5d6de3bb681a5a41e8d01295cc48e
Nikolay Maev aka Trixon <trix.mnr@gmail.com>
2020-06-20 18:11:32 +0300

    Update index.js

--- RCB.JavaScript/ClientApp/store/index.js
+++ RCB.JavaScript/ClientApp/store/index.js
@@ -1,10 +1,10 @@
-import * as LoginStore from "@Store/loginStore";
-import * as PersonStore from "@Store/personStore";
+import * as loginStore from "@Store/loginStore";
+import * as personStore from "@Store/personStore";
 
 // Whenever an action is dispatched, Redux will update each top-level application state property using
 // the reducer with the matching name. It's important that the names match exactly, and that the reducer
 // acts on the corresponding ApplicationState property type.
 export const reducers = {
-    login: LoginStore.reducer,
-    person: PersonStore.reducer
+    login: loginStore.reducer,
+    person: personStore.reducer
 };
